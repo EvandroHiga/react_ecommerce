5797ea78b2c914c57bb40de319275db009dc2a43
Higa <evandro_higa@hotmail.com>
2021-10-16 18:05:32 -0300

    finalizacao do proj.

--- src/page/Cart/index.js
+++ src/page/Cart/index.js
@@ -1,24 +1,43 @@
+import { useState } from "react";
 import { useCart } from "../../providers/cart";
 
 function Cart(){
     const { cartProducts } = useCart();
-    
+
+    const [costumer, setCostumer] = useState({
+        name:"",
+        address:"",
+    });
+
+    const handleInput = (event) => {
+        setCostumer((previousState) => ({...previousState, [event.target.id]: event.target.value}))
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(`Ola, ${costumer.name}. Obrigado por comprar conosco!`);
+    };
+
     return(
         <>
-            <h3>Cart</h3>
+            <h3>[ My cart ]</h3>
+            
             {cartProducts.map((p) => (
                 <div>
-                    <span>{p.name}</span>
-                    <span>{p.price}</span>
+                    <span>{p.name}</span> <br />
+                    <span>$ {p.price}</span> <br />
+                    <span>===================</span>
                 </div>
             ))}
             
-            <form >
+            <form onSubmit={handleSubmit}>
                 <div>
-                    <label for="name"> Name </label> <input id="name" type="text"/>
+                    <label for="name"> Name </label>
+                    <input id="name" type="text" value={costumer.name} onChange={handleInput} />
                 </div>
                 <div>
-                    <label for="address"> Address </label><input id="address" type="text" />
+                    <label for="address"> Address </label>
+                    <input id="address" type="text" value={costumer.address} onChange={handleInput} />
                 </div>
                 <button class="button" type="submit">Purchase</button>
             </form>
